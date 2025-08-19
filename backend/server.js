579e9efba409f97481715a177c635dfa154b45a1
Pranav Mattipi <pranavmattipi@Pranavs-MacBook-Pro.local>
2025-08-19 19:24:23 +0530

    Postman Format

--- backend/server.js
+++ backend/server.js
@@ -34,3 +34,25 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+
+//STUDENT FORMAT
+//http://localhost:8000/api/students/add
+// {
+//   "name": "ParamM",
+//   "rollNumber": "CSE2025026",
+//   "branch": "Computer Science",
+//   "year": 2,
+//   "email": "se24ucse144@iitd.ac.in",
+//   "password": "helloworld123",
+//   "collegeId": "68a333cfa5fc739f0ea5ccd5"
+// }
+
+
+//COLLEGE FORMAT
+//http://localhost:8000/api/colleges/add
+// {
+//   "name": "Mahindra University",
+//   "domain": "mahindrauniversity.edu.in",
+//   "acquired": false
+// }
